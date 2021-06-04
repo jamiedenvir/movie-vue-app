4a92fb0589b0eba8b37ff0fcc3cc4025d7ff0d73
jamie denvir <jwdenvir@gmail.com>
2021-06-04 09:59:11 -0400

    creates movie vue app to practice vue crud

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>New Movie</h2>
+    <input type="text" v-model="newMovieTitle" placeholder="Title" />
+    <br />
+    <input type="text" v-model="newMovieYear" placeholder="Year" />
+    <br />
+    <input type="text" v-model="newMoviePlot" placeholder="Plot" />
+    <br />
+    <input type="text" v-model="newMovieDirector" placeholder="Director" />
+    <br />
+    <input type="text" v-model="newMovieEnglish" placeholder="English?" />
+    <br />
+
+    <button v-on:click="createMovie()">Add Movie</button>
+
+    <div v-for="movie in movies" :key="movie.id">
+      <button v-on:click="showInfo(movie)">More Info</button>
+      <h3>Title: {{ movie.title }}</h3>
+      <p>Year: {{ movie.year }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+      <p>Director: {{ movie.director }}</p>
+    </div>
+    <dialog id="movie-details">
+      <form method="dialog">
+        <h1>Movie Info!</h1>
+        <p>Title: {{ currentMovie.title }}</p>
+        <p>Year: {{ currentMovie.year }}</p>
+        <p>Plot: {{ currentMovie.plot }}</p>
+        <p>Director: {{ currentMovie.director }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      movies: [],
+      currentMovie: {},
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMoviePlot: "",
+      newMovieDirector: "",
+      newMovieEnglish: "",
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
+    createMovie: function () {
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot,
+        director: this.newMovieDirector,
+        english: this.newMovieEnglish,
+      };
+      axios
+        .post("http://localhost:3000/movies", params)
+        .then((response) => {
+          console.log("You've done it!", response.data);
+          this.movies.push(response.data);
+          this.newMovieTitle = "";
+          this.newMovieYear = "";
+          this.newMoviePlot = "";
+          this.newMovieDirector = "";
+          this.newMovieEnglish = "";
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    showInfo: function (movie) {
+      this.currentMovie = movie;
+      console.log(movie);
+      document.querySelector("#movie-details").showModal();
+    },
   },
 };
 </script>
